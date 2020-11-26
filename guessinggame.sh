b25098adbdf22417ebf5151a68c1d565fc34dfe9
Thanhha456 <thanhha.le8@gmail.com>
2020-11-26 12:32:25 +0100

    edited guessinggame

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 function guessinggame {
+    num=$(ls -1 |wc -l) 
+
     echo "Welcome to Guessinggame!"
     echo "Please enter how many files there are in the current directory"
     read response
     echo "You entered: $response"
-    while [[ $response -ne 3 ]]
+    while [[ $response -ne $num ]]
     do
-        if [[ $response -gt 3 ]] 
+        if [[ $response -gt $num ]] 
         then
            echo "This is too high. Please guess again"
            read response
-        elif  [[ $response -lt 3 ]] 
+        elif  [[ $response -lt $num ]] 
         then
             echo "This is too low. Please guess again"
             read response
         fi
     done
-    if [[ $response -eq 3 ]] 
+    if [[ $response -eq $num ]] 
     then
          echo "Congratulations! You guessed it."
     fi 
 }
 
+guessinggame
