97d09a9496ba7b0928f01bdf3b718f106fd0d458
Thanhha456 <thanhha.le8@gmail.com>
2020-11-28 22:05:58 +0100

    edited guessinggame

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 function guessinggame {
 
-    num=$(git ls-files | wc -l)     
+    num=$(find ~/Guessinggame -type f | wc -l)       
     echo "Welcome to Guessinggame!"
     echo "Please enter how many files there are in the current directory."
     read response
     echo "You entered: $response"
 
-    while ! [[ $response -eq $num ]]
+    while true
     do
         if [[ $(echo $response | egrep -q "^[0-9]+$"; echo $?) -ne 0 ]];
         then
              echo "Must be a number. Please guess again."
-       
         elif [[ $response -lt $num ]]
         then 
               echo "This is too low. Please guess again."
-        else
+        elif [[ $response -gt $num ]]
+        then
              echo "This is too high. Please guess again."
+        else
+            echo "Congratulations! You guessed it."
+            break
         fi
+
         read response
         echo "You entered: $response"
     done
-    echo "Congratulations! You guessed it."
 
 
 }
